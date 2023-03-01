a51453405f42902022ec1f3c38decbe31dc7a1b7
tristanblc <tblanc.sio@gmail.com>
2023-03-01 18:19:35 +0100

    initialize tests

---
+++ 2DGameMonogame/2DGameMonogame.Tests/Service/ServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacmanMonogame.Services;
+
+namespace _2DGameMonogame.Tests
+{
+    public class  ServiceTest
+    {
+        private IService _service;
+
+
+        public ServiceTest()
+        {
+            _service= new Service();
+        }
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            
+        }
+
+        [TestMethod]
+        public void SaveKeyInJsonTest()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void ReadSavedKeysMenuTest()
+        {
+
+        }
+    }
+}
